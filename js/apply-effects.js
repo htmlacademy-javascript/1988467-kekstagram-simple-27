9ddfc5d6844a08a80455a5ca47416f21b52f210d
Julia Antoshkina <julia.antoshkina@gmail.com>
2022-11-10 21:33:57 +0100

    Added changing the level of the effect

--- js/apply-effects.js
+++ js/apply-effects.js
@@ -2,7 +2,84 @@ const effectsList = document.querySelector('.effects__list');
 const imageUploadPreview = document.querySelector('.img-upload__preview');
 const image = imageUploadPreview.querySelector('img');
 
+const sliderElement = document.querySelector('.effect-level__slider');
+let effectLevel = 0;
+
 let effectValue;
+
+const EFFECT_SETTING_MAP = {
+  chrome: {
+    updateSliderOptions: {
+      range: {
+        'min': 0,
+        'max': 1,
+      },
+      start: 1,
+      step: 0.1,
+    },
+    effect: (level) => `grayscale(${level})`
+  },
+
+  sepia: {
+    updateSliderOptions: {
+      range: {
+        'min': 0,
+        'max': 1,
+      },
+      start: 1,
+      step: 0.1,
+    },
+    effect: (level) => `sepia(${level})`
+  },
+
+  marvin: {
+    updateSliderOptions: {
+      range: {
+        'min': 0,
+        'max': 100,
+      },
+      start: 100,
+      step: 1,
+    },
+    effect: (level) => `invert(${level}%)`
+  },
+
+  phobos: {
+    updateSliderOptions: {
+      range: {
+        'min': 0,
+        'max': 3,
+      },
+      start: 3,
+      step: 0.1,
+    },
+    effect: (level) => `blur(${level}px)`
+  },
+
+  heat: {
+    updateSliderOptions: {
+      range: {
+        'min': 1,
+        'max': 3,
+      },
+      start: 3,
+      step: 0.1,
+    },
+    effect: (level) => `brightness(${level})`
+  }
+
+};
+
+
+noUiSlider.create(sliderElement, {
+  range: {
+    'min': 0,
+    'max': 100,
+  },
+  start: 100,
+  step: 1,
+  connect: 'lower',
+});
 
 function addEffectsController() {
   effectsList.addEventListener('click', (evt) => {
@@ -13,15 +90,39 @@ function addEffectsController() {
       effectValue = effectItem.value;
     }
 
+    if (effectValue !== 'none') {
+      sliderElement.classList.remove('hidden');
+    } else {
+      sliderElement.classList.add('hidden');
+    }
+
     image.className = '';
     image.classList.add(`effects__preview--${effectValue}`);
+
+
+    const effectSettings = EFFECT_SETTING_MAP[effectValue];
+
+    if (effectSettings) {
+      sliderElement.noUiSlider.updateOptions(effectSettings.updateSliderOptions);
+    }
+
+
+    sliderElement.noUiSlider.on('update', () => {
+      effectLevel = sliderElement.noUiSlider.get();
+      image.style.filter = effectSettings ? effectSettings.effect(effectLevel) : '';
+    });
+
   });
 }
 
-// removeEffectsController для удаления класса у картинки
+// removeEffectsController для удаления эффектов у картинки
 
 function removeEffectsController() {
   image.className = '';
+  sliderElement.noUiSlider.updateOptions({
+    start: 100,
+  });
+  image.style.filter = 'none';
 }
 
 export { addEffectsController, removeEffectsController };
